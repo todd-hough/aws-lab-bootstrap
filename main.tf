15f439cea0b5fd574c1811012924cd1f7c2f5a3b
todd-hough <todd@hough.fastmail.com>
2026-02-02 16:04:22 -0600

    cleanup AWS resource names

--- main.tf
+++ main.tf
@@ -87,8 +87,8 @@ resource "aws_route_table_association" "public" {
 }
 
 # Security Group for EC2
-resource "aws_security_group" "dev_instance" {
-  name        = "${var.project_name}-dev-instance-sg"
+resource "aws_security_group" "server_sg" {
+  name        = "${var.project_name}-server-sg"
   description = "Security group for development EC2 instance"
   vpc_id      = aws_vpc.main.id
 
@@ -115,7 +115,7 @@ resource "aws_security_group" "dev_instance" {
 
   tags = merge(
     {
-      Name = "${var.project_name}-dev-instance-sg"
+      Name = "${var.project_name}-server-sg"
     },
     var.tags
   )
--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "instance_id" {
   description = "EC2 instance ID"
-  value       = aws_instance.dev_instance.id
+  value       = aws_instance.server.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address (Elastic IP) of the EC2 instance"
-  value       = aws_eip.dev_instance_eip.public_ip
+  value       = aws_eip.server_eip.public_ip
 }
 
 output "instance_private_ip" {
   description = "Private IP address of the EC2 instance"
-  value       = aws_instance.dev_instance.private_ip
+  value       = aws_instance.server.private_ip
 }
 
 output "vpc_id" {
@@ -25,23 +25,23 @@ output "subnet_id" {
 
 output "security_group_id" {
   description = "Security group ID"
-  value       = aws_security_group.dev_instance.id
+  value       = aws_security_group.server_sg.id
 }
 
 output "ssh_private_key" {
   description = "Private SSH key to connect to the instance (SAVE THIS SECURELY!)"
-  value       = tls_private_key.dev_ssh_key.private_key_pem
+  value       = tls_private_key.server_ssh_key.private_key_pem
   sensitive   = true
 }
 
 output "ssh_connection_command" {
   description = "SSH command to connect to the instance"
-  value       = "ssh -i dev-key.pem ec2-user@${aws_eip.dev_instance_eip.public_ip}"
+  value       = "ssh -i dev-key.pem ec2-user@${aws_eip.server_eip.public_ip}"
 }
 
 output "cloudwatch_log_group" {
   description = "CloudWatch log group name"
-  value       = var.enable_cloudwatch_logs ? aws_cloudwatch_log_group.dev_instance_logs[0].name : "CloudWatch logging disabled"
+  value       = var.enable_cloudwatch_logs ? aws_cloudwatch_log_group.server_logs[0].name : "CloudWatch logging disabled"
 }
 
 output "setup_instructions" {
@@ -53,13 +53,13 @@ output "setup_instructions" {
        chmod 600 dev-key.pem
 
     2. Connect via SSH:
-       ssh -i dev-key.pem ec2-user@${aws_eip.dev_instance_eip.public_ip}
+       ssh -i dev-key.pem ec2-user@${aws_eip.server_eip.public_ip}
 
     3. For VS Code Remote SSH:
        - Install the "Remote - SSH" extension
        - Add to ~/.ssh/config:
-         Host ${var.project_name}-dev
-           HostName ${aws_eip.dev_instance_eip.public_ip}
+         Host ${var.project_name}-server
+           HostName ${aws_eip.server_eip.public_ip}
            User ec2-user
            IdentityFile /path/to/dev-key.pem
        - Connect using Command Palette: "Remote-SSH: Connect to Host"
